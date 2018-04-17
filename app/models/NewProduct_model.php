f670b8d8c05969e064cb4fc72ad76c70e80aa390
Gustaf Lindholm <babiluskus@gmail.com>
2018-04-17 22:07:49 +0200

    Models and views for new product and variants

--- app/models/NewProduct_model.php
+++ app/models/NewProduct_model.php
@@ -13,15 +13,19 @@ class NewProduct_model extends Base_model
             ':info' => $_POST['newProd']['info'],
             ':manufacturer' => $_POST['newProd']['manufacturer'],
         ];
-        
+
         if($this->prepQuery($this->sql, $paramBinds))
         {
-            // empties the newProd post array
+            // empty the newProd post array
             $_POST['newProd'] = [];
 
             // set a success status which indicates that a new product was added
             $_POST['newProdStatus'] = 'success';
+
+            // send last inserted id
+            $_POST['newProdId'] = $this->lastInsertId;
             return true;
+            
         } else {
 
             // set a failed status that indicates a failure with the insertion of new product
--- app/views/AddProduct.view.php
+++ app/views/AddProduct.view.php
@@ -1,9 +1,64 @@
-<form action="" method="POST">
-    <label for="newProd[title]">Product Title/Name</label>
-    <input type="text" name="newProd[title]">
-    <label for="newProd[info]">Product Description</label>
-    <textarea name="newProd[info]">Describe the product</textarea>
-    <label for="newProd[manufacturer]">Manufacturer</label>
-    <input type="text" name="newProd[manufacturer]">
-    <button type="submit">Save</button>
-</form>
+<style>
+    /* Temporary styling */
+    .container {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+    }
+
+    form {
+        grid-column: 2 / 4;
+        margin: auto;
+    }
+
+    button {
+        display: block;
+        margin: 5% auto 0 auto;
+    }
+</style>
+<div class="container">
+    <form action="" method="POST">
+        <fieldset>
+            <legend>Please fill in the requested product information</legend>
+            <p>
+                <label for="newProd[title]">Product Title/Name</label>
+                <input type="text" name="newProd[title]" placeholder="product title">
+            </p>
+            <p>
+                <label for="newProd[info]">Product Description</label>
+                <textarea name="newProd[info]" placeholder="Describe the product"></textarea>
+            </p>
+            <label for="newProd[manufacturer]">Manufacturer</label>
+            <input type="text" name="newProd[manufacturer]" placeholder="manufacturer">
+            <button type="submit">Save</button>
+        </fieldset>
+    </form>
+
+    <!-- show form for new product variant -->
+    <form action="" method="POST">
+        <input type="hidden" name="addVariant[status]" value="true">
+        <a href="">
+            <button type="submit">Add variant</button>
+        </a>
+    </form>
+
+    <!-- Output the last inserted id -->
+    <h4><?php if(isset($_POST['newProdId'])) {echo "The last inserted product id: " . $_POST['newProdId'];} ?></h4>
+    
+    <?php 
+            //var_dump($_POST['newProdId']);
+            // check if user selected the add variant option(pushed button)
+            if(isset($_POST['newProdId']))
+            { var_dump($_SERVER['REQUEST_URI']);?>
+            <!-- Output the last auto incremented product id -->
+    <form action="localhost/projekt_grupp5/public/admin/addVariant" method="POST">
+        <fieldset>
+            <legend>Enter variation info</legend>
+            <input type="number" name="addVariant['product_id']" placeholder="product id">
+            <input type="text" name="addVariant[sku]" placeholder="stock keeping unit">
+            <input type="number" name="addVariant[price]" placeholder="price">
+            <input type="text" name="addVariant[img_url]" placeholder="image url">
+        </fieldset>
+        <button type="submit">Save new variant</button>
+    </form>
+    <?php } ?>
+</div>
