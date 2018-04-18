b129c99502257e30eeba2ad2f36fd95ca619affa
Carolina <you@example.com>
2018-04-18 12:22:53 +0200

    users routing working!

--- app/controllers/account.controller.php
+++ app/controllers/account.controller.php
@@ -3,7 +3,7 @@
 class Account extends base_controller 
 {
 
-    public function index($fname = "", $lname = "", $phone = "", $email = "") //here shall the users url be so this is completly wrong
+    public function index($uid = "") //the users id url displays here 
     {
         //echo "account index";
 
@@ -11,7 +11,7 @@ class Account extends base_controller
         $this->initModel('Account_model');
         //var_dump($this->modelObj);
 
-        $data = $this->modelObj->getPerson($fname, $lname, $phone, $email);
+        $data = $this->modelObj->getPerson($uid);
 
         //connects controller with the right view
         $this->reqView('account',$data);
--- app/models/Account_model.php
+++ app/models/Account_model.php
@@ -2,14 +2,14 @@
 class Account_model extends Base_model
 {
        
-    // query the database for one persons id passed in the url, so it's wrong now
-    public function getPerson($fname, $lname, $phone, $email)
+    // query the database for one persons id passed in the url
+    public function getPerson($uid) 
     {
         $this->sql = 
-        "SELECT fname, lname, phone, email FROM projekt_klon.user"; //here you should get both the clients info and include the password
+        "SELECT * FROM user JOIN account ON account.uid = user.uid WHERE user.uid = :uid"; // both the clients info and password
         
-        // parameters to be bound, is sent to the prepQuery method
-        $paramBinds = ['fname' => $fname, 'lname' => $lname, 'phone' => $phone, 'email' => $email];
+        // parameters to be bound, is sent to the prepQuery method (doesn't always have to be included)
+        $paramBinds = [':uid' => $uid];
         $base = new Base_model;
         
         $base->prepQuery($this->sql, $paramBinds);
