5a50662141b975b2a2f4fccb01ddc7ad6c1fa4dc
Gustaf Lindholm <babiluskus@gmail.com>
2018-04-19 15:34:33 +0200

    Improved form-generating class. Adds bootstrap classes

--- app/inc/Form.inc.php
+++ app/inc/Form.inc.php
@@ -2,14 +2,22 @@
 
 class Form
 {
-    private $output;
+    private $output = [];
 
     public function textInput($name, $pholder="", $lable, $inputClass = "form-control", $value="")
     {
         $label = "<lable for='$name'>$lable</lable>";
         $input = "<input class='$inputClass' type='text' name='$name' value='$value' placeholder='$pholder'>";
         $element = $label . $input;
-        $this->output .= $element;
+        $this->output[] = $element;
+    }
+
+    public function textAreaInput($name, $pholder="", $lable, $inputClass = "form-control", $value="")
+    {
+        $label = "<lable for='$name'>$lable</lable>";
+        $input = "<textarea class='$inputClass' name='$name' value='$value'>$pholder</textarea>";
+        $element = $label . $input;
+        $this->output[] = $element;
     }
     
     public function numInput($name, $pholder="", $lable)
@@ -30,14 +38,28 @@ class Form
 
     public function button($value, $type ="submit", $class="btn btn-primary")
     {
-        $button = "<button type='$type' class='$class'>$value</button>";
-        $this->output .= $button;
+        $element = "<button type='$type' class='$class'>$value</button>";
+        $this->output[] = $element;
     }
 
-    public function render($action, $method)
+    // method to render all the elements in $this->output
+    public function render($action, $method = 'POST')
     {
         echo "<form action='$action' method='$method'>";
+
+        // loop to add each input element between div with bootstrap form-group class
+        // skips button elements
+        foreach ($this->output as $key => $value) {
+            
+            if(substr_compare($value, '<button', 0, 6)){
+            $this->output[$key] = '<div class="form-group">'.$value.'</div>';
+            }
+        }
+        // create a string with html
+        $this->output = implode("", $this->output);
+
         echo $this->output;
+
         echo "</form>";
     }
 }
