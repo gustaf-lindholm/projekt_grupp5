b57f5dbb8676e8877d2db6ff3b51542affc28767
Gustaf Lindholm <babiluskus@gmail.com>
2018-04-20 15:43:09 +0200

    Fixar buggar

--- app/controllers/products.controller.php
+++ app/controllers/products.controller.php
@@ -2,13 +2,13 @@
 
 class Products extends Base_controller
 {
-    public function index($pid)
+    public function index()
     {
         // instansiate new model using the function built in from the Base Controller
         $this->initModel('Products_model');
 
         //We request modelObjs from the database
-        $data = $this->modelObjs->getAllProducts();
+        $data = $this->modelObj->getAllProducts();
 
         //This will be shown on our products page
         $this->reqView('Products', $data);
