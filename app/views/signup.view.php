36d6dc19134ea7d98a7b614283835071fd1b3810
Musa Jallow <37367336+musajallow@users.noreply.github.com>
2018-04-20 10:27:25 +0200

    Labels
    La bara till labels

--- app/views/signup.view.php
+++ app/views/signup.view.php
@@ -5,11 +5,17 @@
 		<h2>Signup</h2>
 		<form class="signup-form" action="/app/models/Signup_model.php" method="POST">
 			<input type="hidden" name="[submit][level_id]" value="1">
+			<label for="firstname-input">Firstname <span class="text-danger">*</span></label>
 			<input type="text" name="[submit][fname]" placeholder="Firstname">
+			<label for="lastnamename-input">Lastname <span class="text-danger">*</span></label>
 			<input type="text" name="[submit][lname]" placeholder="Lastname">
+			<label for="email-input">Email <span class="text-danger">*</span></label>
 			<input type="text" name="[submit][email]" placeholder="E-mail">
+			<label for="telephone-input">Telephone <span class="text-danger">*</span></label>
 			<input type="text" name="[submit][phone]" placeholder="Telephone">
+			<label for="username-input">Username <span class="text-danger">*</span></label>
 			<input type="text" name="[submit][username]" placeholder="Username">
+			<label for="password-input">Password <span class="text-danger">*</span></label>
 			<input type="text" name="[submit][password]" placeholder="Password">
 			<button type="submit" name="submit">Sign up</button>
 		</form>
