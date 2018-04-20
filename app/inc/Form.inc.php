a0d95f8fdb993b30d24401b8ddc52c7b37ad70bf
Gustaf Lindholm <babiluskus@gmail.com>
2018-04-20 13:14:49 +0200

    Improvements

--- app/inc/Form.inc.php
+++ app/inc/Form.inc.php
@@ -12,20 +12,20 @@ class Form
         $this->output[] = $element;
     }
 
-    public function textAreaInput($name, $pholder="", $lable, $inputClass = "form-control", $value="")
+    public function textAreaInput($name, $pholder="", $lable, $inputClass = "form-control")
     {
         $label = "<lable for='$name'>$lable</lable>";
-        $input = "<textarea class='$inputClass' name='$name' value='$value'>$pholder</textarea>";
+        $input = "<textarea class='$inputClass' name='$name' placeholder='$pholder'></textarea>";
         $element = $label . $input;
         $this->output[] = $element;
     }
     
-    public function numInput($name, $pholder="", $lable)
+    public function numInput($name, $pholder="", $lable, $inputClass = "form-control", $value="")
     {
         $label = "<lable for='$name'>$lable</lable>";
-        $input = "<input type='number' name='$name' placeholder='$pholder'>";
+        $input = "<input class='$inputClass' type='number' name='$name' placeholder='$pholder'>";
         $element = $label . $input;
-        $this->output .= $element;
+        $this->output[] = $element;
     }
 
     public function emailInput($name, $pholder="", $lable)
@@ -33,7 +33,7 @@ class Form
         $label = "<lable for='$name'>$lable</lable>";
         $input = "<input type='email' name='$name' placeholder='$pholder'>";
         $element = $label . $input;
-        $this->output .= $element;
+        $this->output[] = $element;
     }
 
     public function button($value, $type ="submit", $class="btn btn-primary")
@@ -43,10 +43,10 @@ class Form
     }
 
     // method to render all the elements in $this->output
-    public function render($action, $method = 'POST')
+    public function render($action,  $legend, $method = 'POST')
     {
         echo "<form action='$action' method='$method'>";
-
+        echo "<legend>$legend</legend>";
         // loop to add each input element between div with bootstrap form-group class
         // skips button elements
         foreach ($this->output as $key => $value) {
@@ -59,7 +59,6 @@ class Form
         $this->output = implode("", $this->output);
 
         echo $this->output;
-
         echo "</form>";
     }
 }
--- app/views/AddProduct.view.php
+++ app/views/AddProduct.view.php
@@ -6,15 +6,18 @@
     }
 
     form {
+        display: grid;
+        grid-column: 2 / 4;
+    }
+
+    h4 {
         grid-column: 2 / 4;
         margin: auto;
     }
-
-    button {
-        display: block;
-        margin: 5% auto 0 auto;
-    }
 </style>
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+</head>
 <div class="container">
 
         <?php
@@ -25,36 +28,25 @@
         $ProdForm->textInput('newProd[manufacturer]','Manufacturer','Product Manufacturer');
         $ProdForm->button('Save');
         $action = URLrewrite::adminURL('addproduct');
-        $ProdForm->render($action);
+        $ProdForm->render($action,'Add Product Information');
+        
         ?>
-
-    <!-- show form for new product variant -->
-    <form action="<?php echo URLrewrite::adminURL('addvariant') ?>" method="POST">
-        <input type="hidden" name="addVariant[status]" value="true">
-        <a href="">
-            <button type="submit">Add variant</button>
-        </a>
-    </form>
 
             <!-- Output the last auto incremented product id -->
     <h4><?php if(isset($_POST['newProdId'])) {echo "The last inserted product id: " . $_POST['newProdId'];} ?></h4>
     
     <?php 
-            // check if user selected the add variant option(pushed button)
+            //show add variant form after new product has been added
             if(isset($_POST['newProdId']))
-            { ?>
+            {
+                $variantForm = new Form;
+                $variantForm->textInput('addVariant[product_id]', 'Product Id', 'Product ID');
+                $variantForm->textInput('addVariant[sku]', 'SKU', 'Stock Keeping Unit');
+                $variantForm->numInput('addVariant[price]', 'Price', 'Product Price');
+                $variantForm->textInput('addVariant[img_url]', 'Image Url', 'Url to product image');
+                $variantForm->button('Save Variant');
+                $action = URLrewrite::adminURL('addvariant');
+                $variantForm->render($action,'Add Variant Information');
 
-            <!-- Sets the form action to addVariant method in admin -->
-            <!-- htmlentities converts html characters to html entities -->
-    <form action="<?php echo preg_replace('~addproduct~', 'addvariant', htmlentities($_SERVER['REQUEST_URI'])); ?>" method="POST">
-        <fieldset>
-            <legend>Enter variation info</legend>
-            <input type="number" name="addVariant[product_id]" placeholder="product id">
-            <input type="text" name="addVariant[sku]" placeholder="stock keeping unit">
-            <input type="number" name="addVariant[price]" placeholder="price">
-            <input type="text" name="addVariant[img_url]" placeholder="image url">
-        </fieldset>
-        <button type="submit">Save new variant</button>
-    </form>
-    <?php } ?>
-</div>
+            }
+    ?>
