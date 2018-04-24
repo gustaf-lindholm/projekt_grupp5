a4f12cb52e7ce8f72f9a12cbe4faf6702d6f8b71
Carolina <you@example.com>
2018-04-24 13:12:36 +0200

    orderhistory working and displaying from DB

--- app/models/Orderhistory_model.php
+++ app/models/Orderhistory_model.php
@@ -1,6 +1,14 @@
 <?php
-
-
-
-
-?>
+class Orderhistory_model extends Base_model
+{
+    public function displayOrders($uid)
+    {
+        $this->sql = 
+        "SELECT * FROM projekt_klon.order JOIN product_variants ON projekt_klon.order.pid = product_variants.product_id 
+        AND projekt_klon.order.variant_id = product_variants.variant_id WHERE projekt_klon.order.uid = :uid";
+        $binds = [':uid' => $uid];
+        $this->prepQuery($this->sql, $binds);
+        $data = $this->getAll();
+        return $data;
+    }
+}
