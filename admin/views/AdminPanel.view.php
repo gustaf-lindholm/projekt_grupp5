8af17af642db930a1cf8f81ccd8e0212e6e35a74
Gustaf Lindholm <babiluskus@gmail.com>
2018-04-25 22:15:20 +0200

    Added temporary admin menu

--- admin/views/AdminPanel.view.php
+++ admin/views/AdminPanel.view.php
@@ -1,5 +1,7 @@
 <div>
         <a href="<?php echo URLrewrite::URL('addproduct') ?>"><button id="addProduct" class="btn btn-primary adminButton">Add New Product</button></a>
+        <a href="<?php echo URLrewrite::URL('productoptions') ?>"><button id="addProduct" class="btn btn-primary adminButton">Manage Product Options</button></a>
+        
 </div>
 
 
--- admin/views/ProdOptions.view.php
+++ admin/views/ProdOptions.view.php
@@ -1,3 +1,8 @@
+<div>
+        <p>Temporary menu</p>
+        <a href="<?php echo URLrewrite::BaseAdminURL('addproduct') ?>"><button id="addProduct" class="btn btn-primary adminButton">Add New Product</button></a>
+        <a href="<?php echo URLrewrite::BaseAdminURL('productoptions') ?>"><button id="addProduct" class="btn btn-primary adminButton">Manage Product Options</button></a>      
+</div>
 <div class="form-container">
 <?php
     $products = $data[0];
