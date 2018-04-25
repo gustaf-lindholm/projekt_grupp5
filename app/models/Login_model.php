9c4e6cffce439e018a665de427de3b81db57020e
Musa Jallow <37367336+musajallow@users.noreply.github.com>
2018-04-25 15:26:46 +0200

    Login & Signup fix

--- app/models/Login_model.php
+++ app/models/Login_model.php
@@ -9,35 +9,33 @@ class Login_model extends Base_model
 	public function login()
     {
         $_SESSION['loggedIn'] = false;
-    	$User->loginUser([
-            'username' => $_POST['username'], 
-            'password' => $_POST['password']);
-
-        // kolla om alla fälten är ifyllda
+    	// kolla om alla fälten är ifyllda
         if (empty($_POST['username']) || empty($_POST['password'])) {
         	echo "Fyll i de tomma fälten";
         } else { // kolla så att alla fälten är korrekt ifyllda
         	if (!preg_match('/^[a-zA-Z]*$/', $_POST['username'])) {
         		echo "fel tecken";
-        } else { // kolla så att det är rätt epostadress
-        	if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-            			echo "invalid email";
+            } else { // kolla så att det är rätt epostadress
+            	if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                			echo "invalid email";
             		} else { //hasha och kolla om lösenord matchar
             			$username = $_POST['username'];
             			$hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
-            			$sql = "SELECT username,password FROM account WHERE username = :username AND  password = :hashedPassword");
+            			$sql = "SELECT username,password FROM account WHERE username = :username AND  password = :hashedPassword";
                         $paramBinds = [':username' => $username, ':password' => $hashedPassword];
                         prepQuery($sql, $paramBinds = []);
-						$result = query($sql);
+    					$result = query($sql);
             			$resultCheck = mysql_fetch_row($result);
             			if ($resultCheck == 0) { //om det är fel så omdirigera användaren tillbaka till formuläret
             				echo "du finns inte i db, försök igen";
             			} else { //om rätt användare finns i db, logga in användaren 
             				if ($resultCheck['username'] === $username && $resultCheck['username'] === $hashedPassword) {
                                 $_SESSION['loggedIn'] = true;
-                                
+                                URLrewrite::URL('account');
                             }
             		}
+                }
+            }
         }
     }
 }
--- app/models/Signup_model.php
+++ app/models/Signup_model.php
@@ -5,21 +5,10 @@ class Signup_Model extends Base_model
 	// här bygger jag olika metoder som kan hantera och göra olika saker
     public function signup()
     {
-    	    $User = new User;
-            $userId = $User->createUser([
-                'level_id' => $_POST['level_id'],
-                'fname' => $_POST['fname'],
-                'lname' => $_POST['lname'],
-                'email' => $_POST['email'],
-                'phone' => $_POST['phone'],
-                'username' => $_POST['username'],
-                'password' => $_POST['password']
-            ]);
-
-            if (empty($_POST['fname']) || empty($_POST['lname']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['username']) || empty($_POST['password'])) {
+    	    if (empty($_POST['fname']) || empty($_POST['lname']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['username']) || empty($_POST['password'])) {
             	echo "du måste fylla i de tomma fälten";
             } else {
-            	if (!preg_match('/^[a-zA-Z]*$/', $_POST['fname']) || !preg_match('/^[a-zA-Z]*$/', $_POST['lname'])) || !preg_match('/^[a-zA-Z]*$/', $_POST['username'])) {
+            	if (!preg_match('/^[a-zA-Z]*$/', $_POST['fname']) || !preg_match('/^[a-zA-Z]*$/', $_POST['lname']) || !preg_match('/^[a-zA-Z]*$/', $_POST['username'])) {
             		echo "fel tecken";
             	} else {
             		if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
