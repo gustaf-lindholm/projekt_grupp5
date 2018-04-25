fbccd780d13c5f677c020e97fc8ba1071df2ec1e
Musa Jallow <37367336+musajallow@users.noreply.github.com>
2018-04-25 14:40:33 +0200

    Login
    update

--- app/models/Login_model.php
+++ app/models/Login_model.php
@@ -8,10 +8,10 @@ class Login_model extends Base_model
 	
 	public function login()
     {
+        $_SESSION['loggedIn'] = false;
     	$User->loginUser([
-                'username' => $_POST['username'],
-                'password' => $_POST['password']
-            );
+            'username' => $_POST['username'], 
+            'password' => $_POST['password']);
 
         // kolla om alla fälten är ifyllda
         if (empty($_POST['username']) || empty($_POST['password'])) {
@@ -25,13 +25,18 @@ class Login_model extends Base_model
             		} else { //hasha och kolla om lösenord matchar
             			$username = $_POST['username'];
             			$hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
-            			$sql = "SELECT username,password FROM account WHERE username = '".$username."' AND  password = '".$hashedPassword."'");
+            			$sql = "SELECT username,password FROM account WHERE username = :username AND  password = :hashedPassword");
+                        $paramBinds = [':username' => $username, ':password' => $hashedPassword];
+                        prepQuery($sql, $paramBinds = []);
 						$result = query($sql);
             			$resultCheck = mysql_fetch_row($result);
             			if ($resultCheck == 0) { //om det är fel så omdirigera användaren tillbaka till formuläret
             				echo "du finns inte i db, försök igen";
-            			} else { //om det är rätt, logga in användaren
-            				$this->controllerObj->login();
+            			} else { //om rätt användare finns i db, logga in användaren 
+            				if ($resultCheck['username'] === $username && $resultCheck['username'] === $hashedPassword) {
+                                $_SESSION['loggedIn'] = true;
+                                
+                            }
             		}
         }
     }
