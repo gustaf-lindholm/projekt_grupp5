4c91a7891bcbf1737128065d4043eb059563515f
Musa Jallow <37367336+musajallow@users.noreply.github.com>
2018-05-02 17:52:41 +0200

    User update
    get all user
    get one user

--- app/models/User_model.php
+++ app/models/User_model.php
@@ -12,11 +12,13 @@ class User_model extends Base_model
         return self::$data;
     }
 
-    public function getUser()
+    public function getUser($uid)
     {
     	$this->sql = 
-        "SELECT user.uid, user.level_id, user.fname, user.lname, user.phone, user.email, user_levels.level_type FROM user
-        INNER JOIN  user_levels ON user.level_id = user_levels.level_id";
+        "SELECT user.uid, user.level_id, user.fname, user.lname, user.phone, user.email, user_levels.level_type, account.username FROM user 
+		INNER JOIN user_levels ON user.level_id = user_levels.level_id
+		INNER JOIN account ON user.uid = account.uid 
+		WHERE user.uid = :uid AND account.username = username";
         $this->prepQuery($this->sql);
         $this->getOne();
         return self::$data;
