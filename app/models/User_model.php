62951dad65db8600c81e78400e40f05a0aca7bac
Musa Jallow <37367336+musajallow@users.noreply.github.com>
2018-05-02 13:40:28 +0200

    Userupdate

--- app/models/User_model.php
+++ app/models/User_model.php
@@ -2,13 +2,23 @@
 
 class User_model extends Base_model
 {
-    public function displayAllUsers()
+    public function getAllUsers()
     {
         $this->sql = 
         "SELECT user.uid, user.level_id, user.fname, user.lname, user.phone, user.email, user_levels.level_type FROM user
         INNER JOIN  user_levels ON user.level_id = user_levels.level_id";
         $this->prepQuery($this->sql);
         $this->getAll();
+        return self::$data;
+    }
+
+    public function getUser()
+    {
+    	$this->sql = 
+        "SELECT user.uid, user.level_id, user.fname, user.lname, user.phone, user.email, user_levels.level_type FROM user
+        INNER JOIN  user_levels ON user.level_id = user_levels.level_id";
+        $this->prepQuery($this->sql);
+        $this->getOne();
         return self::$data;
     }
 }
