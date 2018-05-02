76a365486111727e0bbe3f47fecd573a1a33da28
Musa Jallow <37367336+musajallow@users.noreply.github.com>
2018-05-02 13:41:11 +0200

    User update

--- app/controllers/user.controller.php
+++ app/controllers/user.controller.php
@@ -1,40 +1,28 @@
 <?php
 
-class User extends Base_controller
+/**
+* 
+*/
+class User extends Base_Controller
 {
-
-    static protected $table_name = "users";
-    static protected $db_columns = ['uid', 'user_level', 'fname', 'lname', 'phone', 'email'];
-
-    public $uid;
-    public $user_level;
-    public $fname;
-    public $lname;
-    public $username;
-    protected $hashed_password;
-    public $password;
-    public $confirm_password;
-
-
-    // public function __construct($args=[])
-    // {
-    //      // $this->fname= $args['fname'] ?? '';
-    //     // $this->fname= $args['lname'] ?? '';
-    //     // $this->username= $args['username'] ?? '';
-    //     // $this->password= $args['password'] ?? '';
-    //     // $this->confirm_password= $args['confirm_password'] ?? '';
-    // }
-
-    public function show_Full_Name()
+    
+    public function index()
     {
+        // instansiate new model using the function built in from the Base Controller
         $this->initModel('User_model');
 
-         //We request modelObjs from the database
-         $data = $this->modelUsers->getAllProducts();
-
-         //This will be shown on our products page
-         $this->reqView('Products', $data);
- 
+        //We request modelObjs from the database
+        $data = $this->modelObj->getAllUsers();
     }
+
+    public function getUser() 
+    {
+        // instansiate new model using the function built in from the Base Controller
+        $this->initModel('User_model');
+
+        //We request modelObjs from the database
+        $data = $this->modelObj->getUser();
+    }
+
 
 }
