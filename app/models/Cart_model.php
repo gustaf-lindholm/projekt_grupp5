81731d54f024b4096e1924c13f96584166786049
Musa Jallow <37367336+musajallow@users.noreply.github.com>
2018-05-11 12:34:44 +0200

    Cart update

--- app/models/Cart_model.php
+++ app/models/Cart_model.php
@@ -8,7 +8,9 @@ class Cart_model extends Base_model
 	
 	public function cartItems()
 	{
-		// get the array with chosen product(s) from session
+		// get the array with chosen product(s) from session with or without session_username set
+		var_dump($_POST['cartItem']);
+		$cartItems = $_POST['cartItem'];
 		// show product(s) from session
 		// alter product(s)
 		// save final product list to session
