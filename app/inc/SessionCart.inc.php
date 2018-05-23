3b3dc9f312b366302c3c3d7f28768384cc4c05c4
Musa Jallow <37367336+musajallow@users.noreply.github.com>
2018-05-23 17:41:04 +0200

    mmm

--- app/inc/SessionCart.inc.php
+++ app/inc/SessionCart.inc.php
@@ -8,21 +8,11 @@ class SessionCart {
 	public function addProduct($sku, $amount = 1) 
 	{
 	
-<<<<<<< HEAD
 		if (array_key_exists($sku, $this->products)) {
 			$this->products[$sku] += $amount;
 		} else {
 			$this->products[$sku] = $amount;
 		}
-=======
-		// if (array_key_exists($pid, $this->products) {
-		// 	$this->products[$pid] += $amount;
-		// } else {
-		// 	$this->products[$pid] = $amount;
-		// }
-
-
->>>>>>> 270f7f359f363fc2aa1339f5f213b11309ecdd04
 	}	
 
 	public function remove($sku) 
