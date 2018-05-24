63be2aa64180c1d763d94caf27f80f7182be6973
saranguatsogtoo <sarangua97@gmail.com>
2018-05-24 15:06:15 +0200

    Toggle header

--- app/views/header.php
+++ app/views/header.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="<?php echo URLrewrite::BaseURL().'css/product.css';?>" type="text/css"/>
     <link rel="stylesheet" href="<?php echo URLrewrite::BaseURL().'css/products.css';?>" type="text/css"/>
     <link rel="stylesheet" href="<?php echo URLrewrite::BaseURL().'css/grid_table.css';?>" type="text/css"/>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
     
@@ -29,6 +29,7 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li><a href="<?php echo URLrewrite::BaseURL().'products'?>">Products<span class="sr-only">(current)</span></a></li>
+
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">About Us<span class="caret"></span></a>
           <ul class="dropdown-menu">
@@ -36,20 +37,36 @@
           </ul>
         </li>
       </ul>
+    
+
 
       <form class="navbar-form navbar-left">
         <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
+          <input type="text" autocomplete="off" class="form-control" placeholder="Search" onkeyup="searchMobile();">
+          <div class="result"></div>
         </div>
-        <button type="submit" class="btn btn-default">Search</button>
       </form>
-      
-      <a href="<?php echo URLrewrite::BaseURL().'login'?>"><button class="btn btn-success" type="btn">Login</button></a>
-<?php
-    if (isset($_SESSION['loggedIn']['username'])) { ?>
-    <a href="<?php echo URLrewrite::BaseURL().'login/logout'?>"><button class="btn btn-success" type="btn">Logout</button></a>
-<?php }?>
-      <a href="<?php echo URLrewrite::BaseURL().'companysignup'?>"><button class="btn btn-success" type="btn">Company Login</button></a>
+   
+
+      <form class= "navbar-form navbar-right">
+          <ul class="nav navbar-nav">
+              <li class="dropdown">
+              <a href="<?php echo URLrewrite::BaseURL().'login'?>"class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><button class="btn btn-success" type="btn">Login</button></a>
+          
+          <ul class="dropdown-menu">
+            <?php
+              if (isset($_SESSION['loggedIn']['username'])) { ?>
+              <li><a href="<?php echo URLrewrite::BaseURL().'login/logout'?>">Logout</a></li>
+            <?php }?>
+
+              <li><a href="<?php echo URLrewrite::BaseURL().'login'?>" >Account</a></li>
+              <li><a href="<?php echo URLrewrite::BaseURL().'signup'?>" >Sign Up</a></li>
+              <li><a href="<?php echo URLrewrite::BaseURL().'companysignup'?>" >Company</a></li>
+          </ul>
+              </li>
+          </ul>
+      </form>
+
           </div><!-- End for .navbar-collapse -->
 </div><!-- End for -navbar-header-->
 </div><!-- End for .container-fluid -->
