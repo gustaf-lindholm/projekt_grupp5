7b3c1b78b91f0087743924c2c771eafff771601e
saranguatsogtoo <sarangua97@gmail.com>
2018-06-11 09:43:25 +0200

    Checkout Part 4

--- app/views/checkout.view-1.php
+++ app/views/checkout.view-1.php
@@ -10,29 +10,35 @@ if ($email === false) {
 }
 
 ?>
+<div class=" container">
 <h1>Fill in Your Information</h1>
 <div class="form-row">
-                        <div class="form-group col-md-6">
+                        <div class="mb-3 control-group">
                         <label for="user[first_Name]">Firstname: </label>
                         <input type="text" name="user[first_Name]" class="form-control" value="<?php echo $first_name?>" id="resultOfFirstName" placeholder="<?php echo $first_name?>"  required>
                         </div>
 
-                        <div class="form-group col-md-6">
+                        <div class="form-group mb-3 ">
                         <label for="user[last_Name]">Surname: </label>
                         <input type="text" class="form-control"  name="user[last_Name]" id="lastName" value="<?php echo $last_name?>" placeholder="<?php echo $last_name?>"  required>
                         </div>
 </div>
 
 <div class="form-row">
-                        <div class="form-group col-md-6">
+                        <div class="form-group mb-3 ">
                         <label for="user[email_Address]">Email Adress: </label>
                         <input type="email" class="form-control" name="user[email_Address]"  value="<?php echo $email_Address?>" placeholder="<?php echo $email_Address?>" id="resultOfEmailAddress" size="50" required/>
                         </div>
 
-                        <div class="form-group col-md-6">
+                        <div class="form-group mb-3 ">
                         <label for="user[telephone_Number]">Telephone: </label>
                         <input type="tel" class="form-control" name="user[telephone_Number]" value="<?php echo $telephone_Number?>" placeholder="<?php echo $telephone_Number?>"  id="resultOfPhoneNumber" size="50" required/>
                         </div>
+
+                        <label for="user[consent_Checkbox]">
+                        <input type="checkbox" name="user[consent_Checkbox]" value="new_Member">
+                        Create an account</label>
+
 </div>
 
 
@@ -40,23 +46,21 @@ if ($email === false) {
 <?php 
 if(isset($_SESSION['loggedIn'])) {
 ?>
+    <div class="control-group">			
     <input type="hidden" name="stage" value="<?= $stage + 2 ?>"/>
-    <input type="submit" value="Next"/>
+    <input type="submit" class="btn btn-success" value="Continue"/>
+    </div>
     </form>
 <?php
 }else{
 ?>
-                         <div class="form-group col-md-12">
-                        <label for="user[consent_Checkbox]">
-                        <input type="checkbox" name="user[consent_Checkbox]" class="form-control" value="new_Member">
-                        Create an account
-                        </label>
-                        </div>
-</div>
-
+<div class="control-group">			
 <input type="hidden" name="stage" value="<?= $stage + 1 ?>"/>
-<input type="submit" value="Next"/>
+<input type="submit" class="btn btn-success" value="Continue"/>
+</div>
 </form>
+
 <?php 
 }
 ?>
+</div>
