65de7639587a17acb6f123a9de3fedde47fb18bc
saranguatsogtoo <sarangua97@gmail.com>
2018-06-11 12:22:14 +0200

    Sanitize & Filter

--- app/views/checkout.view-1.php
+++ app/views/checkout.view-1.php
@@ -1,30 +1,55 @@
 <form action="<?= URLrewrite::BaseURL().'checkout'?>" method='post'>
 <?php include __DIR__."/checkout_details.view.php"; ?>
 
-<?php
-/*
- if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    echo("$email is a valid email address");
-  } else {
-    echo("$email is not a valid email address");
-  }
-*/
-?>
-
 <div class=" container">
 <h1>Fill in Your Information</h1>
 
+
 <div class="form-row">
-                        <div class="mb-3 control-group">
+<?php
+
+if(!(isset($_POST['user']['first_Name']) && (strlen($_POST['user']['first_Name'])>3))) {
+    // $_SESSION['user']['first_Name'] = filter_var($_POST['user']['first_Name'], FILTER_SANITIZE_STRING);
+    //echo "Enter a first name of at least 3 letters";
+?>
+
+                         <div class="mb-3 control-group">
                         <label for="user[first_Name]">Firstname: </label>
                         <input type="text" name="user[first_Name]" class="form-control" value="<?php echo $first_name?>" id="resultOfFirstName" placeholder="<?php echo $first_name?>"  required>
                         </div>
+<?php 
+}
 
-                        <div class="form-group mb-3 ">
+if(! (isset($_POST['user']['last_Name']) && (strlen($_POST['user']['last_Name'])>3))) {
+    // $_SESSION['user']['last_Name'] = filter_var($_POST['user']['last_Name'], FILTER_SANITIZE_STRING);
+    //echo "Enter a last name of at least 3 letters";
+    ?>
+           <div class="form-group mb-3 ">
                         <label for="user[last_Name]">Surname: </label>
                         <input type="text" class="form-control"  name="user[last_Name]" id="lastName" value="<?php echo $last_name?>" placeholder="<?php echo $last_name?>"  required>
                         </div>
 </div>
+
+<?php 
+}
+
+/*
+ if (filter_var($_POST['user']['email_Address'], FILTER_VALIDATE_EMAIL)) {
+    echo("This is a valid email address");
+  } else {
+    echo("This is not a valid email address");
+  }
+
+  if (filter_var($_POST['user']['telephone_Number'],  FILTER_SANITIZE_NUMBER_INT)) {
+    echo("This is a valid telephone_Number");
+  } else {
+    echo("This is not a telephone_Number");
+  }
+
+*/ 
+?>
+
+             
 
 <div class="form-row">
                         <div class="form-group mb-3 ">
