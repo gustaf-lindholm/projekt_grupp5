3c6f1fb28f84a1b8dc265d27acfd4b3d81ca0856
Gustaf Lindholm <babiluskus@gmail.com>
2018-06-12 11:59:55 +0200

    working

--- app/views/checkout.view.php
+++ app/views/checkout.view.php
@@ -23,7 +23,7 @@ include __DIR__."/checkout.view-$stage.php";
 
 echo "<div class='col-md-12'><pre>";
 var_dump($_SESSION);
-var_dump($_POST);
+var_dump(unserialize($_POST['order_set']['quantity']));
 echo "</pre>";
 
 
